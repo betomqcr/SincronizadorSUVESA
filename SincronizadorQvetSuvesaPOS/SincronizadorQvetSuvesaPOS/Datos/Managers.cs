75162466542d43b457d21379cf4c9514e4e70965
Alberto Martínez Quesada <betomqcr@gmail.com>
2022-08-02 16:02:21 -0600

    Creacion de Clase Managers

--- SincronizadorQvetSuvesaPOS/SincronizadorQvetSuvesaPOS/Datos/Managers.cs
+++ SincronizadorQvetSuvesaPOS/SincronizadorQvetSuvesaPOS/Datos/Managers.cs
@@ -3,10 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SincronizadorQvetSuvesaPOS.Datos;
 
 namespace SincronizadorQvetSuvesaPOS.Datos
 {
-    internal class Managers
+    
+    public  class Managers
     {
+        public SINCRONIZADOREntities entities;
+        public Managers()
+        {
+            entities = new SINCRONIZADOREntities();
+        }
+
+        public int InsertarAlbaranes()
+        {
+            try
+            {
+
+            }
+            catch(Exception ex)
+            {
+
+            }
+            return 0; 
+        }
+
+
     }
 }
