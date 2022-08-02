e46d7af6eae61364fd4cb3fcbb687026db0cfbe9
Alberto Martínez Quesada <betomqcr@gmail.com>
2022-08-02 15:39:47 -0600

    Creacion de Negocio y Datos

---
+++ SincronizadorQvetSuvesaPOS/SincronizadorQvetSuvesaPOS/Negocio/Proccess.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SincronizadorQvetSuvesaPOS.Negocio
+{
+    internal class Proccess
+    {
+    }
+}
