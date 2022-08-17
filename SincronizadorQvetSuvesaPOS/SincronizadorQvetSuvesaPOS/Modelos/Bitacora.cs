eb30dbebe75f888576164539c1b6fbaa6efb49ca
Carlos Pereira <carloscoto0103@gmail.com>
2022-08-16 20:17:59 -0600

    Se actualiza el modelo de BD

---
+++ SincronizadorQvetSuvesaPOS/SincronizadorQvetSuvesaPOS/Modelos/Bitacora.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SincronizadorQvetSuvesaPOS.Modelos
+{
+    public static class Bitacora
+    {
+        public static string UsuarioSuvesa { get; set; }
+
+        public static string Accion { get; set; }
+
+        public static string Observaciones { get; set; }
+
+    }
+}
